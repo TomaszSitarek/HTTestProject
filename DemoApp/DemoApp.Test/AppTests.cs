d89b95a96b3fb72c986d1055ec74c2029ed3e95b
Tomasz Sitarek <xxrst@eg.dk>
2018-10-18 14:38:41 +0200

    Add demo app

---
+++ DemoApp/DemoApp.Test/AppTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoApp.Test
+{
+    [TestClass]
+    public class AppTests
+    {
+        [TestMethod]
+        public void Items_Ordered() => Assert.IsTrue(MainWindow.Items.SequenceEqual(MainWindow.Items.OrderBy(x => x)));
+    }
+}
---
+++ DemoApp/DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string[] Items = new[] {"a","b","c" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ComboBox.ItemsSource = Items;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.TextBlock.Text = this.ComboBox.SelectedItem.ToString();
+        }
+    }
+}
